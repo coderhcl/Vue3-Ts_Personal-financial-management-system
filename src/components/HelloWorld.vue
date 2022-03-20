9ee83c9fa6f830bdd4ed26e2e43e72f9b2c9e4f9
coderhcl <2814884095@qq.com>
2022-03-20 21:46:22 +0800

    test01

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -111,13 +111,17 @@
 </template>
 
 <script lang="ts">
+
+
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "HelloWorld",
+
   props: {
+
     msg: String,
-  },
+  }
 });
 </script>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: HomeView
   },
   {
     path: "/about",
@@ -14,13 +14,13 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue")
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes
 });
 
 export default router;
