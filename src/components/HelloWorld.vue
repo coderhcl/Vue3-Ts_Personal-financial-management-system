e16635c7c3d6fa1a2ca41e7b4e16d800a7abcecb
coderhcl <2814884095@qq.com>
2022-03-20 21:58:00 +0800

    style(login): login style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -111,16 +111,11 @@
 </template>
 
 <script lang="ts">
-
-
 import { defineComponent } from "vue";
-
 export default defineComponent({
   name: "HelloWorld",
-
   props: {
-
-    msg: String,
+    msg: String
   }
 });
 </script>
